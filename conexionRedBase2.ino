729a66d3297e64e962ab1dd36ca8572103d942ca
DiegoGithubUpla <33336628+DiegoGithubUpla@users.noreply.github.com>
2017-11-11 12:19:51 -0300

    Add files via upload

---
+++ conexionRedBase2.ino
@@ -0,0 +1,109 @@
+#include <WiFiEsp.h> //Libreria para comunicarse facilmente con el modulo ESP01
+#include <SoftwareSerial.h> //Libreria para poder emular un puerto serial
+SoftwareSerial Serial1(3, 2); // RX | TX
+char ssid[] = "matilda2";            // SSID (Nombre de la red WiFi)
+char pass[] = "mi24an16die17dia6";        // Contraseña
+int status = WL_IDLE_STATUS;     // Estado del ESP. No tocar
+String strhost = "192.168.1.40";//o server
+String strurl = "/tutorial/conexion.php";
+char server[] = "192.168.1.40"; //Servidor al que nos conectaremos
+char key[] = "";    //Api key
+ 
+WiFiEspClient client;  //Iniciar el objeto para cliente 
+ //////////////////////////////7
+boolean conectaTJ;
+boolean conectaBD;
+int var;
+//////////////////////////////////7
+void setup()
+{
+  
+  Serial.println("paso por aqui 2");
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  WiFi.init(&Serial1);
+ do{
+    conectaTJ=true;
+    if (WiFi.status() == WL_NO_SHIELD) {
+      Serial.println("EL VALOR DE WL_NO_SHIELD ES: ");
+      Serial.println(WL_NO_SHIELD);
+      Serial.println("Modulo no presente. Reinicie el Arduino y el ESP01 (Quite el cable que va de CH_PD a 3.3V y vuelvalo a colocar)");
+      //Loop infinito 
+     }else{
+      Serial.println("SE HA DETECTADO EL MODULO");
+      conectaTJ=false;
+     }
+ }while(conectaTJ);    
+  //Intentar conectar a la red wifi
+
+ do{
+    Serial.print("Intentando conectar a la red WiFi: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    Serial.print("lectura:");//2//2//2//2//1 paso
+    Serial.println(status);
+    client.connected();
+}while(status != WL_CONNECTED);
+
+
+do{
+  var=client.connect(server, 8080);
+  conectaBD=true;
+  if(var==1){
+    conectaBD=false;
+  }
+}while(conectaBD);
+  
+}
+
+void loop(){
+      String datos="chipid=123&temperatura=123";
+//////////////////////////////////BORRAME//////////////////////////////////
+
+        client.print(String("POST ") + strurl + " HTTP/1.1" + "\r\n" + 
+               "Host: " + strhost + "\r\n" +
+               "Accept: */*" + "*\r\n" +
+               "Content-Length: " + datos.length() + "\r\n" +
+               "Content-Type: application/x-www-form-urlencoded" + "\r\n" +
+               "\r\n" + datos);
+        datos="chipid=12&temperatura=12";     
+        client.print(String("POST ") + strurl + " HTTP/1.1" + "\r\n" + 
+               "Host: " + strhost + "\r\n" +
+               "Accept: */*" + "*\r\n" +
+               "Content-Length: " + datos.length() + "\r\n" +
+               "Content-Type: application/x-www-form-urlencoded" + "\r\n" +
+               "\r\n" + datos);                        
+               
+          delay(10);             
+          
+          Serial.print("Enviando datos a SQL...");
+
+        
+
+  /*
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  
+  }
+ */
+  //Desconexion
+  Serial.println(var);
+  if (!client.connected()) {//entra si ocurre un error
+    Serial.println();
+    Serial.println("Desconectando del servidor...");
+    client.stop();
+  }
+    // Reintentar
+    Serial.println();
+    Serial.println("Reintentar? <y>/<n>");
+    boolean y=false;
+    while(y==false)
+    {
+      String x = Serial.readString();
+      x.trim();
+      y=(x=="y" ? true : false);
+    }
+    
+  }
+   
---
+++ unionServoRif.ino
@@ -0,0 +1,80 @@
+#include "SPI.h"
+#include "MFRC522.h"
+
+#define SS_PIN 10
+#define RST_PIN 9
+#define SP_PIN 8
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+#include <Servo.h>
+ 
+// Declaramos la variable para controlar el servo
+Servo servoMotor;
+MFRC522::MIFARE_Key key;
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
+  servoMotor.attach(4);
+}
+int cont=1;
+void loop() {
+  Serial.println("entro al loop");
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
+    return;
+
+  // Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  // Serial.println(rfid.PICC_GetTypeName(piccType));
+
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return;
+  }
+
+  String strID = "";
+  for (byte i = 0; i < 4; i++) {
+    strID +=
+    (rfid.uid.uidByte[i] < 0x10 ? "0" : "") +
+    String(rfid.uid.uidByte[i], HEX) +
+    (i!=3 ? ":" : "");
+  }
+  strID.toUpperCase();
+  Serial.print("Tap card key: ");
+  Serial.println(strID);
+  if(strID=="66:5C:24:7E"){
+    Serial.print("\nEntre al sector\n");
+    cont++;
+    Serial.print(cont);
+    /*-----------------------------------------------*/
+    if(cont%2==0){
+      //si es 1,3,5,7,9
+      // Desplazamos a la posición 90º
+  servoMotor.write(90);
+  // Esperamos 1 segundo
+  delay(1000);
+    }else{
+      //si 2,4,6,8,10
+      servoMotor.write(0);
+  // Esperamos 1 segundo
+  delay(1000);
+    }
+      
+  
+  
+  
+  // Desplazamos a la posición 180º
+  //servoMotor.write(180);
+  // Esperamos 1 segundo
+  //delay(1000);
+    /*-----------------------------------------------*/
+    }else{
+      Serial.print("\nAcceso no permitido a esta area\n");
+    }
+
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
+}
